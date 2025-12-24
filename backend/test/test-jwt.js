e46bbbf6243db31c8c6f40096fd3b2e2bbb049d6
Navarasan N S M <nsmnavarasan@gmail.com>
2025-12-24 20:58:41 +0530

    Delete backend/test directory

--- backend/test/test-jwt.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('../src/services/jwt.service');
-
-console.log('=== JWT Service Info ===');
-console.log('Algorithm:', jwt.algorithm);
-console.log('Service file:', require.resolve('./src/services/jwt.service'));
-console.log('Has getPublicJwk:', typeof jwt.getPublicJwk === 'function');
-console.log('====================');
